d481bcf63a336e584eed636149b055e5662d8237
domrachev-andrey <andrey@domrachev.ru>
2023-01-04 16:28:32 +0300

    04.01.23 16:28
    04.01.23 16:28

--- articles/index.php
+++ articles/index.php
@@ -7,6 +7,10 @@
 				<iframe width="560" height="315" src="https://www.youtube.com/embed/t41LIorFhwM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
 				</iframe>
 			</p> 
+			<p>
+				<a href="https://vk.com/video-217010365_456239018">VK</a>
+				<a href="https://rutube.ru/video/809c3023929f6948206164ba175c79f9/">RuTube</a>
+			</p>
 		</div>
 	</main>
 <?php
