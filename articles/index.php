987685716d49348aa2a1154e5a0adc3549aac0df
domrachev-andrey <andrey@domrachev.ru>
2023-01-10 22:07:41 +0300

    10.01.23 22:07
    10.01.23 22:07

--- articles/index.php
+++ articles/index.php
@@ -14,6 +14,16 @@
 				<a href="https://rutube.ru/video/809c3023929f6948206164ba175c79f9/">RuTube</a>
 				<a href="https://dzen.ru/video/watch/63b577e87fc57c5d5d472409?share_to=link">Дзен</a>	
 			</p>
+			<h4>Урок 2. Создание учебной базы 1С</h4>
+			<p>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/iyNW4dOyR2A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+				</iframe>
+			</p>
+			<p>
+				<a href="https://vk.com/video-217010365_456239020">VK</a>
+				<a href="https://rutube.ru/video/b7cec40a3f3ab374f34ad20176cce5bf/">RuTube</a>
+				<a href="https://dzen.ru/video/watch/63bd762e491ee17bd9de6cc1?share_to=link">Дзен</a>	
+			</p>
 		</div>
 	</main>
 <?php
