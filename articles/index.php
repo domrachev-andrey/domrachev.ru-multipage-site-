0646f31117ffa9567cb3638c6e65f9d10dcad664
domrachev-andrey <andrey@domrachev.ru>
2023-02-11 18:41:49 +0300

    11.02.23 18:41
    11.02.23 18:41

--- articles/index.php
+++ articles/index.php
@@ -81,7 +81,7 @@
 			</p>
 			<p>
 				<a href="https://vk.com/video-217010365_456239029">VK</a>
-				<a href="https://rutube.ru/video/9411b22b9eaef7d6ec30944e12169d79/">RuTube</a>
+				<a href="https://rutube.ru/video/c20bbf884fc3fb9040cf7b36d55f2d52/">RuTube</a>
 				<a href="https://dzen.ru/video/watch/63e2459f00d5cb197f3a3c6e?share_to=link">Дзен</a>	
 			</p>
 		</div>
