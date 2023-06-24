2e3270c35f03b45238b0723f55a328598f6ed2e5
domrachev-andrey <andrey@domrachev.ru>
2023-06-24 18:36:07 +0300

    24.06.23 18:35
    24.06.23 18:35

--- interview/index.php
+++ interview/index.php
@@ -15,7 +15,15 @@
 				<a href="https://rutube.ru/video/d0fb17c0a95e588386bb63cf8a297650/">RuTube</a>
 				<a href="https://dzen.ru/video/watch/6485f7eed940955d100fbeac?share_to=link">Дзен</a>	
 			</p>
-			
+			<h4>Задача 2. Регистр сведений - последний и предпоследний срез</h4>
+			<p>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/Qg2oB1Rsa0Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			</p> 
+			<p>
+				<a href="https://vk.com/video-217010365_456239039">VK</a>
+				<a href="https://rutube.ru/video/d0fb17c0a95e588386bb63cf8a297650/">RuTube</a>
+				<a href="https://dzen.ru/video/watch/6485f7eed940955d100fbeac?share_to=link">Дзен</a>	
+			</p>
 		</div>
 	</main>
 <?php
