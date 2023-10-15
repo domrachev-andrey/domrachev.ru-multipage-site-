61a32ac048af4297eedb310527e8b42c4e18c52e
domrachev-andrey <andrey@domrachev.ru>
2023-10-15 23:47:13 +0600

    15.10.23 23:47
    15.10.23 23:47

--- articles/index.php
+++ articles/index.php
@@ -167,9 +167,9 @@
 			<p>
 				<iframe width="560" height="315" src="https://www.youtube.com/embed/eLtGIChReJ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 			<p>
-				<a href="https://vk.com/video-217010365_456239043">VK</a>
+				<a href="https://vk.com/video-217010365_456239044">VK</a>
 				<a href="https://rutube.ru/video/644740a6d0ae7dc6c80c966a040fc2b7/">RuTube</a>
-				<a href="https://dzen.ru/video/watch/64cfb5e62b27db0318bfa7d3">Дзен</a>	
+				<a href="https://dzen.ru/video/watch/652c1f5bd141d60240c4ba95">Дзен</a>	
 			</p>
 
 		</div>
