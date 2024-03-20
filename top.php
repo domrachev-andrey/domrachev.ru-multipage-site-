9e173e97fb4125a0c58a83849759577546d4f635
domrachev-andrey <andrey@domrachev.ru>
2024-03-20 11:55:47 +0300

    20.03.24 11:55
    20.03.24 11:55

--- top.php
+++ top.php
@@ -53,6 +53,20 @@
 		<div><img src="https://mc.yandex.ru/watch/90960295" style="position:absolute; left:-9999px;" alt="" /></div>
 	</noscript>
 	<!-- /Yandex.Metrika counter -->
+<!-- Top.Mail.Ru counter -->
+<script type="text/javascript">
+var _tmr = window._tmr || (window._tmr = []);
+_tmr.push({id: "3494724", type: "pageView", start: (new Date()).getTime(), pid: "USER_ID"});
+(function (d, w, id) {
+  if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+  ts.src = "https://top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+})(document, window, "tmr-code");
+</script>
+<noscript><div><img src="https://top-fwz1.mail.ru/counter?id=3494724;js=na" style="position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div></noscript>
+<!-- /Top.Mail.Ru counter -->
 	<title><?php echo isset($pageTitle) ? $pageTitle : '1С Франчайзи Домрачев консалтинг'; ?></title>
 </head>
 
